f0d700eae3d2ce3e414fb69adf18901cff6d629a
shamelessUser <amaamma9@gmail.com>
2023-07-26 13:28:51 +0400

    add server.php and modified modal window logic

--- js/script.js
+++ js/script.js
@@ -98,16 +98,27 @@ window.addEventListener("DOMContentLoaded", function () {
     setClock(".timer", "2023-07-17 22:00:00");
     // timer logic end
 
-    // modal logic star
+    // modal logic start
     const modalTrigger = document.querySelectorAll("[data-modal]");
     const modal = document.querySelector(".modal");
-    const modalCloseBtn = document.querySelector("[data-close]");
 
     modalTrigger.forEach(btn => btn.addEventListener("click", openModal));
-    modalCloseBtn.addEventListener("click", closeModal);
+
+    function closeModal() {
+        modal.classList.remove("show");
+        modal.classList.add("hide");
+        document.body.removeAttribute("style");
+    }
+
+    function opemModal() {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        //clearTimeout(modalTimerId);
+    }
 
     modal.addEventListener("click", (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute("data-close") == "") {
             closeModal();
         }
     });
@@ -163,9 +174,9 @@ window.addEventListener("DOMContentLoaded", function () {
         changeToUAH() {
             this.price = +this.price * this.transfer;
         }
-    
+
         render() {
-            const {img, alt, title, descr, price, parent} = this;
+            const { img, alt, title, descr, price, parent } = this;
             const element = document.createElement("div");
             element.classList.add("menu__item");
             element.innerHTML = `
@@ -178,7 +189,7 @@ window.addEventListener("DOMContentLoaded", function () {
                     <div class="menu__item-total"><span>${price}</span> грн/день</div>
                 </div>
             `;
-    
+
             parent.append(element);
         }
     };
@@ -211,4 +222,139 @@ window.addEventListener("DOMContentLoaded", function () {
     ).render();
 
     // used Class for meu cards => end
+
+    const fomrs = document.querySelectorAll("form");
+
+    function spinner() {
+        return `
+            <?xml version="1.0" encoding="utf-8"?>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="38px" height="38px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <g transform="rotate(0 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(30 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(60 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(90 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(120 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(150 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(180 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(210 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(240 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(270 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(300 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+                </rect>
+            </g><g transform="rotate(330 50 50)">
+                <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#337ab7">
+                    <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+                </rect>
+            </g>
+            <!-- [ldio] generated by https://loading.io/ --></svg>
+        `;
+    }
+
+    const messages = {
+        loading: spinner,
+        success: "Thank you ! We will contact with you !",
+        failure: "Sorry, but something went wrong !"
+    };
+
+    fomrs.forEach(form => postData(form));
+
+    function  postData (form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const {loading, success, failure} = messages;
+
+            const loader = document.createElement("div");
+            loader.innerHTML = loading();
+            form.append(loader);
+
+            const request = new XMLHttpRequest();
+            request.open("POST", "server.php");
+
+            //request.setRequestHeader("multipart/form-data");
+            request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+            if (!navigator.onLine) {
+                messagesModal(failure + ": " + "Please check your internet connection, and try again!");
+                loader.remove();
+                form.reset();
+            }
+
+            const formData = new FormData(form);
+
+            const obj = {};
+            formData.forEach((val, key) => obj[key] = val);
+
+            request.send(JSON.stringify(obj));
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    messagesModal(success);
+                    loader.remove();
+                    form.reset();
+                } else {
+                    messagesModal(failure);
+                    loader.remove();
+                    form.reset();
+                }
+            });
+        });
+    }
+
+    function messagesModal (message) {
+        const prevModalDialog = document.querySelector(".modal__dialog");
+        prevModalDialog.classList.add("hide");
+        openModal();
+
+        const messageModal = document.createElement("div");
+        messageModal.classList.add("modal__dialog");
+        messageModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close> &times; </div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector(".modal").append(messageModal);
+
+        setTimeout(() => {
+            messageModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            closeModal();
+        }, 2000);
+    }
 });
